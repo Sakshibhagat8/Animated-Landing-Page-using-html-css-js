12979fdcc3c3e785bb4072dfd286793b7b4cd46f
Sakshi bhagat <89642424+Sakshibhagat8@users.noreply.github.com>
2024-02-09 01:40:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+gsap.to(".imagecontainer",{
+    ease:Expo.easeInOut,
+    width:"100%",
+    duration:2,
+    stagger:2
+
+})
+
+gsap.to(".text h1",{
+    ease:Expo.easeInOut,
+   
+    stagger:2,
+    top:0,
+
+})
+gsap.to(".text h1",{
+    ease:Expo.easeInOut,
+    delay:2,
+
+    stagger:2,
+    top:"-100%"
+
+})
